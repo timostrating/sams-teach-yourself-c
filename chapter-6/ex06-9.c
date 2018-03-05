fe66d0ae8d5444c1ad0af48f0f2866680a7367ce
Timo Strating <timostrating@home.nl>
2018-03-05 23:55:14 +0100

    finished chapter 6

---
+++ chapter-6/ex06-9.c
@@ -0,0 +1,8 @@
+int main(void) {
+    int record = 0; // record = 0;
+    while (record = 100)
+    {
+        printf( "\nRecord %d", record);
+        printf( "\nGetting next nmber..." );
+    }
+}
