328f2ea0c5e83df6acdbd36382178adfc8eb78eb
timostrating <timostrating@home.nl>
2018-05-14 11:33:24 +0200

    finished chapter 14

---
+++ chapter-14/ex14-3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main() {
+    char buffer[31];
+    scanf("%30[^*]", buffer);
+}
---
+++ chapter-14/ex14-4.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void main() {
+    puts("Jack asked \"What is a backslash?\"\nJill said, \"It is '\\'\"");
+}
